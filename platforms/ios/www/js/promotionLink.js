007fc9376d922291095f34a51ec47397a39c5544
Muhammad Raza <mraza8040@gmail.com>
2019-04-11 10:05:45 -0700

    commit add by umer with raza

--- platforms/ios/www/js/promotionLink.js
+++ platforms/ios/www/js/promotionLink.js
@@ -1,12 +1,1 @@
-function openLink(object) {
-		//count ads 
-var y = localStorage.getItem("counterAds");
-var x  = 1;
-
-var sum = (x*100 + y*100) / 100;
- localStorage.setItem("counterAds",sum);
-	//End count ads 
-// initApp();
-var webLink = $(object).attr('webLink');
-window.open(webLink,"_blank");
-}
+function openLink(e){var t=(100+100*localStorage.getItem("counterAds"))/100;localStorage.setItem("counterAds",t);var n=$(e).attr("webLink");window.open(n,"_blank")}
